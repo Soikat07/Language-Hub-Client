91b82fe0a91bd61d3e1ca364f77662bdd24a7909
Soikat07 <sahasoikat94@gmail.com>
2023-06-10 08:23:05 +0600

    useInstructor hook done

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,12 +1,13 @@
 import { FaBook, FaCalendarAlt, FaHome, FaWallet } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useAdmin from "../hooks/useAdmin";
+import useInstructor from "../hooks/useInstructor";
 
 
 const Dashboard = () => {
-  
   const [isAdmin] = useAdmin();
-
+  const [isInstructor] = useInstructor();
+  
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -38,7 +39,18 @@ const Dashboard = () => {
                 </NavLink>
               </li>
             </>
-          ) : (
+          ) : isInstructor ? <>
+              <li>
+                <NavLink to="/dashboard/addClass">
+                  <FaWallet></FaWallet> Add a Class
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/myClasses">
+                  <FaBook></FaBook> My Classes
+                </NavLink>
+              </li>
+            </> : (
             <>
               <li>
                 <NavLink to="/dashboard/selectClasses">
